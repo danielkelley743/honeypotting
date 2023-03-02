7d9e83ae37386f0c19acd49d7d6f09d52c950e6c
Daniel Kelley <86798209+danielkelley743@users.noreply.github.com>
2023-03-02 15:26:13 +0000

    Add files via upload

---
+++ docker/2_run_honeypots_docker_image.sh
@@ -0,0 +1,20 @@
+# Docker image startup script
+# usage: 2_run_honeypots_docker_image.sh <URL for logging>
+
+/etc/init.d/docker start
+
+# Setup desired listening ports
+HTTP_PORT=12345
+FTP_PORT=123
+
+# setup honeypots services ( used in --setup "SERVICES". Ex: http:1234,ftp:5556 )
+SERVICES=http:$HTTP_PORT,ftp:$FTP_PORT
+
+# expose the ports from the docker image to the same port on the machine. !!! each entry must be in the format "-p PORT:PORT". Multiple exposed ports must be separated with spaces
+BIND_PORTS="-p $HTTP_PORT:$HTTP_PORT  -p $FTP_PORT:$FTP_PORT"
+
+# Setup excluded actions. must include escaped double qoutes and separated by commas
+EXCLUDED_ACTIONS=\"get\",\"post\",\"something\"
+
+# run the image
+docker run -e URL=$1 -e SERVICES=$SERVICES -e EXCLUDED_ACTIONS=$EXCLUDED_ACTIONS $BIND_PORTS -it autorunnner00/my-images:start_all-1.0.0
---
+++ honeypots/standalone/start.sh
@@ -0,0 +1,10 @@
+apt-get install python3-pip python3-venv
+echo {\"logs\":\"terminal,url\", \"excluded_actions_from_logging\":[\"post\", \"get\", \"process\"], \"url\":\"$1\"} > config.json
+python3 -m venv .venv
+activate(){
+    . ./.venv/bin/activate
+    pip3 install -r requirements.txt
+    cp ../helper.py .venv/lib/python3.9/site-packages/honeypots
+    honeypots --setup http:1234 --config config.json
+}
+activate
